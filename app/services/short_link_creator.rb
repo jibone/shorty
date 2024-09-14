9f161295dc7d31cc02c5408fe63ff3f165db8995
Jibone <jibone@gmail.com>
2024-09-14 15:24:32 +0800

    Refactor remove the need for short code generator service

--- app/services/short_link_creator.rb
+++ app/services/short_link_creator.rb
@@ -1,5 +1,6 @@
-# TODO: update comment
-# TODO add comment
+require 'digest'
+
+# TODO: add comment
 class ShortLinkCreator
   MAX_ATTEMPTS = 5 # retry 5 time when there is a collision when generating short code.
 
@@ -12,11 +13,19 @@ class ShortLinkCreator
     attempts = 0
     begin
       attempts += 1
-      ShortCodeGenerator.new(@link, seed + attempts).call
+      # ShortCodeGenerator.new(@link, seed + attempts).call
+      @link.short_code = generate_short_code(@link.target_url, seed + attempts)
+      @link
     rescue ActiveRecord::RecordNotUnique
       # If we hit a collision, we keep trying until we reach MAX_ATTEMPTS
       retry if attempts < MAX_ATTEMPTS
       throw ActiveRecord::RecordNotUnique
     end
+  end
+
+  private
+
+  def generate_short_code(target_url, seed)
+    Digest::SHA256.hexdigest("#{target_url}#{seed}")[0..5] # TODO: make this configurable maybe?
   end
 end
--- spec/services/short_code_generator_spec.rb
+++ spec/services/short_code_generator_spec.rb
@@ -1,26 +1,26 @@
-require 'rails_helper'
-
-RSpec.describe ShortCodeGenerator do
-  describe '#call' do
-    let(:link) { Link.new(title: 'Jibone blog', target_url: 'https://jshamsul.com') }
-    let(:seed) { 'random-seed-x' }
-
-    it 'generates a short code' do
-      service = ShortCodeGenerator.new(link, seed)
-      result = service.call
-
-      expect(result.short_code).to be_present
-      expect(result.short_code.length).to eq(6)
-    end
-
-    it 'generate a unique short code based on the target_url and seed' do
-      service1 = ShortCodeGenerator.new(link, 'seed01')
-      service2 = ShortCodeGenerator.new(link, 'seed02')
-
-      short_code1 = service1.call.short_code
-      short_code2 = service2.call.short_code
-
-      expect(short_code1).to_not eq(short_code2)
-    end
-  end
-end
+# require 'rails_helper'
+#
+# RSpec.describe ShortCodeGenerator do
+#   describe '#call' do
+#     let(:link) { Link.new(title: 'Jibone blog', target_url: 'https://jshamsul.com') }
+#     let(:seed) { 'random-seed-x' }
+#
+#     it 'generates a short code' do
+#       service = ShortCodeGenerator.new(link, seed)
+#       result = service.call
+#
+#       expect(result.short_code).to be_present
+#       expect(result.short_code.length).to eq(6)
+#     end
+#
+#     it 'generate a unique short code based on the target_url and seed' do
+#       service1 = ShortCodeGenerator.new(link, 'seed01')
+#       service2 = ShortCodeGenerator.new(link, 'seed02')
+#
+#       short_code1 = service1.call.short_code
+#       short_code2 = service2.call.short_code
+#
+#       expect(short_code1).to_not eq(short_code2)
+#     end
+#   end
+# end
